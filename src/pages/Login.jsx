49870c07aa67875d213cc7b5413020b6e225be2d
yazan-abuhammad <yazanjamalabuhammad@gmail.com>
2023-10-09 11:59:13 +0300

    ok

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React  ,{  useState }from "react";
 import { Link } from "react-router-dom";
 import { Footer, Navbar } from "../components";
 
+import  { useNavigate } from 'react-router-dom';
+import Axios from 'axios';
+
+
 const Login = () => {
+  const navigate = useNavigate
+  const [formData, getFormData] = useState({
+    email: '',
+    password: '',
+});
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        getFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+const handleSubmit = (e) => {
+  e.preventDefault();
+  
+  Axios.post("http://localhost/projectreact/src/DB/useradd.php", formData)
+    .then((response) => {
+      console.log("User added successfully:", response.data);
+
+      getFormData({
+    
+        email: "",
+       
+        password: "",
+      
+      });
+      navigate('/admin/users');
+      
+
+    })
+    .catch((error) => {
+      console.error("Error adding user:", error);
+    });
+  }
+
   return (
     <>
       <Navbar />
@@ -19,6 +59,9 @@ const Login = () => {
                   class="form-control"
                   id="floatingInput"
                   placeholder="name@example.com"
+                  value={getFormData.email}
+                  onChange={handleInputChange}
+                  required
                 />
               </div>
               <div class="my-3">
@@ -28,13 +71,16 @@ const Login = () => {
                   class="form-control"
                   id="floatingPassword"
                   placeholder="Password"
+                  value={getFormData.Password}
+                  onChange={handleInputChange}
+                  required
                 />
               </div>
               <div className="my-3">
                 <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
               </div>
               <div className="text-center">
-                <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
+                <button class="my-2 mx-auto btn btn-dark" type="submit" >
                   Login
                 </button>
               </div>
